f2933eb562b1a6bebb00a79fb52fd99869185093
LucoMoro <morelli.2000luca@gmail.com>
2024-11-25 20:23:38 +0100

    refactor: improve path modularity

--- network/agents.py
+++ network/agents.py
@@ -1,10 +1,11 @@
 from network.huggingface_api import query_huggingface_model
+from network.config import folder_path
 
-with open('../conversations/conversation_1/iteration_1/test_prompt', 'r') as file_in:
+with open(f'{folder_path}conversation_1/iteration_1/test_prompt', 'r') as file_in:
         question = file_in.read()
 
 response = query_huggingface_model(question)
 
 if response:
-    with open('../conversations/conversation_1/iteration_1/response', 'w', encoding='utf-8') as file_out:
+    with open(f'{folder_path}conversation_1/iteration_1/response', 'w', encoding='utf-8') as file_out:
         file_out.write(response[0]["generated_text"])
