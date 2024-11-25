254cbe4d904c4b01e42c0bbde53c5a44bc57ba1f
LucoMoro <morelli.2000luca@gmail.com>
2024-11-25 19:49:24 +0100

    refactor: improve folders structure

--- network/agents.py
+++ network/agents.py
@@ -1,10 +1,10 @@
 from network.huggingface_api import query_huggingface_model
 
-with open('../prompts/test_prompt', 'r') as file_in:
+with open('../conversations/conversation_1/iteration_1/test_prompt', 'r') as file_in:
         question = file_in.read()
 
 response = query_huggingface_model(question)
 
 if response:
-    with open('../conversations/response', 'w', encoding='utf-8') as file_out:
+    with open('../conversations/conversation_1/iteration_1/response', 'w', encoding='utf-8') as file_out:
         file_out.write(response[0]["generated_text"])
