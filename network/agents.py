fdf9292eec372b4e46ef02d1ae624164058a1164
LucoMoro <morelli.2000luca@gmail.com>
2024-12-05 15:00:59 +0100

    refactor: improve path modularity

--- network/agents.py
+++ network/agents.py
@@ -1,11 +1,17 @@
-from network.huggingface_api import query_huggingface_model
-from network.config import folder_path
+import os
 
-with open(f'{folder_path}conversation_1/iteration_1/test_prompt', 'r') as file_in:
+from network.huggingface_api import query_huggingface_model
+from network.config import conversations_path
+from network.utils import ensure_conversation_path, get_iteration_id
+
+iteration_id = get_iteration_id()
+conversation_path = ensure_conversation_path(conversations_path, iteration_id)
+
+with open(f'{conversations_path}conversation_1/iteration_1/test_prompt', 'r') as file_in:
         question = file_in.read()
 
 response = query_huggingface_model(question)
 
 if response:
-    with open(f'{folder_path}conversation_1/iteration_1/response', 'w', encoding='utf-8') as file_out:
+    with open(f'{conversations_path}conversation_1/iteration_{iteration_id}/response', 'w', encoding='utf-8') as file_out:
         file_out.write(response[0]["generated_text"])
--- network/config.py
+++ network/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("HUGGINGFACE_API_KEY")
 
-folder_path = "../conversations/"
+conversations_path = "../conversations/"
 
 model_id = "distilgpt2"
 api_url = f"https://api-inference.huggingface.co/models/{model_id}"
