486b0c1cf5f1700e3f7bdbd6cd7821c4892578ef
LucoMoro <morelli.2000luca@gmail.com>
2024-12-20 01:54:20 +0100

    feat: add try-except in AgentBase constructor

--- network/agents/agent_base.py
+++ network/agents/agent_base.py
@@ -2,35 +2,43 @@ import json
 
 import os
 from contextlib import nullcontext
+from sys import exception
 
 import requests
 from network.config import headers
 
 class AgentBase:
     def __init__(self, file_path):
-        with open(file_path, "r") as agent:
-            agent_data = json.load(agent)
+        try:
+            with open(file_path, "r") as agent:
+                agent_data = json.load(agent)
 
-        self.api_settings = agent_data.get("api_settings", {})
-        self.system_prompt = agent_data.get("system_prompt", {})
-        self.utils = agent_data.get("utils", {})
-        self.default_provider = self.api_settings.get("default_provider", {})
+            self.api_settings = agent_data.get("api_settings", {})
+            self.system_prompt = agent_data.get("system_prompt", {})
+            self.utils = agent_data.get("utils", {})
+            self.default_provider = self.api_settings.get("default_provider", {})
 
-        if self.default_provider == "openai":
-            self.open_ai = self.api_settings.get("openai", {})
-            self.model = self.open_ai.get("model", {})
-            self.endpoint = self.open_ai.get("endpoint", {})
-            self.api_key = self.open_ai.get("api_key", {})
-            self.hugging_face = nullcontext
-        else:
-            self.hugging_face = self.api_settings.get("huggingface", {})
-            self.model = self.hugging_face.get("model", {})
-            self.endpoint = self.hugging_face.get("endpoint", {})
-            self.api_key = self.hugging_face.get("api_key", {})
-            self.open_ai = nullcontext
+            if self.default_provider == "openai":
+                self.open_ai = self.api_settings.get("openai", {})
+                self.model = self.open_ai.get("model", {})
+                self.endpoint = self.open_ai.get("endpoint", {})
+                self.api_key = self.open_ai.get("api_key", {})
+                self.hugging_face = nullcontext
+            else:
+                self.hugging_face = self.api_settings.get("huggingface", {})
+                self.model = self.hugging_face.get("model", {})
+                self.endpoint = self.hugging_face.get("endpoint", {})
+                self.api_key = self.hugging_face.get("api_key", {})
+                self.open_ai = nullcontext
 
-        self.context = self.system_prompt.get("context", {})
-        self.instructions = self.system_prompt.get("instructions", {})
+            self.context = self.system_prompt.get("context", {})
+            self.instructions = self.system_prompt.get("instructions", {})
+
+        except FileNotFoundError:
+            print(f"Error: File {file_path} not found")
+
+        except json.JSONDecodeError:
+            print("Error while reading the JSON file")
 
     def print(self):
         print(self.hugging_face)
@@ -64,5 +72,5 @@ class AgentBase:
         if response.status_code == 200:
             return response.json()
         else:
-            print(f"Errore {response.status_code}: {response.text}")
+            print(f"Error {response.status_code}: {response.text}")
             return None
